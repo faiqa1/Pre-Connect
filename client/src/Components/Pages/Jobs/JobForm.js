9c1c61afc677f8ef2c1c72b395f44b604d4e7214
faiqa1 <faiqarao03@gmail.com>
2024-07-05 22:28:32 +0500

    commit

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -9,6 +9,18 @@ export default function JobForm() {
       <h3>Enter job description</h3> 
       <p>Job title</p>
       <p>Job description</p>
+      <h3>Enter job description</h3> 
+      <p>Job title</p>
+      <p>Job description</p>
+      <h3>Enter job description</h3> 
+      <p>Job title</p>
+      <p>Job description</p>
+      <h3>Enter job description</h3> 
+      <p>Job title</p>
+      <p>Job description</p>
+      <h3>Enter job description</h3> 
+      <p>Job title</p>
+      <p>Job description</p>
     </div>
   )
 }
