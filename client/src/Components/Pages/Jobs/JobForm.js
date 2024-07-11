e7fdee02de3f7aabaf35532d93d24ff1eb330812
faiqa1 <faiqarao03@gmail.com>
2024-07-12 01:49:26 +0500

    Remove three elements from JobForm component.

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function JobForm() {
   return (
     <div>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
+
     </div>
   )
 }
