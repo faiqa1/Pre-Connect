e48efd19c127050afb949c7826cb19a929b3ebaa
faiqa1 <faiqarao03@gmail.com>
2024-07-11 23:54:03 +0500

    "Removed duplicate code blocks in JobForm.js"

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -3,33 +3,6 @@ import React from 'react'
 export default function JobForm() {
   return (
     <div>
-            <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
-      <h3>Enter job description</h3> 
-      <p>Job title</p>
-      <p>Job description</p>
       <h3>Enter job description</h3> 
       <p>Job title</p>
       <p>Job description</p>
