215e119482a38ed51d90449ff3293b625a372715
faiqa1 <faiqarao03@gmail.com>
2024-07-14 06:22:11 +0500

    "Added two forms with headings to JobForm component"

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function JobForm() {
   return (
     <div>
+    <form>
+      <h1>Enter job details</h1>
+    </form>
 
+    <form>
+      <h1>Enter job details</h1>
+    </form>
+    
     </div>
   )
 }
