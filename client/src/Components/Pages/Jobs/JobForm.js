ee7574c4386e57711048158279b88dfad44884d6
faiqa1 <faiqarao03@gmail.com>
2024-07-19 20:13:13 +0500

    git init

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -3,13 +3,7 @@ import React from 'react'
 export default function JobForm() {
   return (
     <div>
-    <form>
-      <h1>Enter job details</h1>
-    </form>
 
-    <form>
-      <h1>Enter job details</h1>
-    </form>
     
     </div>
   )
