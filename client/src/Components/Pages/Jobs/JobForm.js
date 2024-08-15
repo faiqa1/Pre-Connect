a1846019316b31c22555ccffd0c455e31082d5dc
faiqa1 <faiqarao03@gmail.com>
2024-08-15 16:28:46 +0500

    "Added 'Job title' field and rearranged form structure in JobForm component"

--- client/src/Components/Pages/Jobs/JobForm.js
+++ client/src/Components/Pages/Jobs/JobForm.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function JobForm() {
   return (
-    <div><form>
+    <div>
+      <form>
       <div>
         <div>
           <img alt='company-logo'></img>
         </div>
         <div><h3>Enter company name</h3></div>
+        <div>Job title</div>
       </div>
-      </form></div>
+      </form>
+      </div>
   )
 }
