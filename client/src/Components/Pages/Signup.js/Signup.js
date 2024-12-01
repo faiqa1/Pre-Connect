b325a709d364fc730ce08983508ca11cd15043a8
faiqa1 <faiqarao03@gmail.com>
2024-12-02 00:37:32 +0500

    "Updated text in Signup component"

--- client/src/Components/Pages/Signup.js/Signup.js
+++ client/src/Components/Pages/Signup.js/Signup.js
@@ -154,7 +154,7 @@ const Signup = () => {
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Welcome to PreConnect!</h1>
-              <p>Enter your personal details and start journey with us</p>
+              <p>Sign up now and start journey with us</p>
               <button className="ghost " id="signUp" onClick={handleSignUpClick}>
                 Sign Up
               </button>
